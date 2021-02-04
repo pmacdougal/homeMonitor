1c775863bec6791c26bc7787d29142b8e3954121
Paul MacDougal <paulmacd@acm.org>
2021-02-04 17:13:57 -0500

    Add handler for Rriba

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -100,6 +100,22 @@ class Barn(Monitor):
         handler.setup('s.lt', 'T0')
         mqtt_monitor.topic(handler)
 
+        # tele/921601/TRIP {"GCBMS":1.0,"Sketch":"ESP GCBMS coprocessor v1.0","Duration":7,"Odometer":0,"Current":0,"Speed":0,"b0":5231,"B0":5497,"b1":0,"B1":0,"b2":0,"B2":0,"b3":0,"B3":0,"b4":0,"B4":0,"b5":0,"B5":0,"StopTime":159199}
+        handler = Generic('tele/921601/TRIP', metering_queue, 0, 's.mph')
+        handler.NAME = 'Rriba'
+        handler.setup('rriba.b0', 'b0')
+        handler.setup('rriba.b1', 'b1')
+        handler.setup('rriba.b2', 'b2')
+        handler.setup('rriba.b3', 'b3')
+        handler.setup('rriba.b4', 'b4')
+        handler.setup('rriba.b5', 'b5')
+        handler.setup('rriba.duration', 'Duration')
+        handler.setup('rriba.odometer', 'Odometer')
+        handler.setup('rriba.current', 'Current')
+        handler.setup('rriba.speed', 'Speed')
+        mqtt_monitor.topic(handler)
+        )
+
 
 class Home(Monitor):
     '''
